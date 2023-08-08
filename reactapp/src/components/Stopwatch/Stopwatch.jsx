10601164ebaf75ce3af15a72f41394bbc54438d9
Iamneo <techadmin@iamneo.ai>
2023-08-08 04:40:59 +0000

    Backup Commit

--- reactapp/src/components/Stopwatch/Stopwatch.jsx
+++ reactapp/src/components/Stopwatch/Stopwatch.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import classes from './Stopwatch.css'
+import classes from './stopwatch.module.css'
 
 const Stopwatch = () => {
   const [timer, setTimer] = useState(0);
@@ -28,39 +28,37 @@ const Stopwatch = () => {
     setTimer(0);
   };
 
+  const formatTime = time => {
+    const hours = Math.floor(time / 3600).toString().padStart(2, '0');
+    const minutes = Math.floor((time % 3600) / 60).toString().padStart(2, '0');
+    const seconds = (time % 60).toString().padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+  };
+
   return (
     <div>
-      <p data-testid="time" className={classes.time}>
-        {formatTime(timer)}
-      </p>
+      <p data-testid="time">{formatTime(timer)}</p>
       {running ? (
-        <div>
-          <button data-testid="pause" className={classes.button} id="pause" onClick={handleStartPause}>
+        <>
+          <button data-testid="pause" id="pause" onClick={handleStartPause}>
             Pause
           </button>
-          <button data-testid="reset" className={classes.button} id="reset" disabled>
+          <button data-testid="reset" id="reset" onClick={handleReset}>
             Reset
           </button>
-        </div>
+        </>
       ) : (
-        <div>
-          <button data-testid="start" className={classes.button} id="start" onClick={handleStartPause}>
+        <>
+          <button data-testid="start" id="start" onClick={handleStartPause}>
             {timer > 0 ? 'Resume' : 'Start'}
           </button>
-          <button data-testid="reset" className={classes.button} id="reset" onClick={handleReset} disabled={timer === 0}>
+          <button data-testid="reset" id="reset" disabled>
             Reset
           </button>
-        </div>
+        </>
       )}
     </div>
   );
-};
-
-const formatTime = time => {
-  const hours = Math.floor(time / 3600).toString().padStart(2, '0');
-  const minutes = Math.floor((time % 3600) / 60).toString().padStart(2, '0');
-  const seconds = (time % 60).toString().padStart(2, '0');
-  return `${hours}:${minutes}:${seconds}`;
 };
 
 export default Stopwatch;
