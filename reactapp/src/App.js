13016ff3a1bb15d6ad8ce0ba7a6d24e6428156f4
Iamneo <techadmin@iamneo.ai>
2023-08-08 04:20:58 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Stopwatch from './components/Stopwatch';
+import Stopwatch from './components/Stopwatch/Stopwatch';
 
 function App() {
   return (
---
+++ reactapp/src/components/Stopwatch/Stopwatch.jsx
@@ -0,0 +1,64 @@
+// src/components/Stopwatch/Stopwatch.jsx
+import React, { useState, useEffect } from 'react';
+import 
+const Stopwatch = () => {
+  const [timer, setTimer] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (running) {
+      interval = setInterval(() => {
+        setTimer(prevTimer => prevTimer + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [running]);
+
+  const handleStartPause = () => {
+    setRunning(prevRunning => !prevRunning);
+  };
+
+  const handleReset = () => {
+    setRunning(false);
+    setTimer(0);
+  };
+
+  return (
+    <div>
+      <p data-testid="time">{formatTime(timer)}</p>
+      {running ? (
+        <>
+          <button data-testid="pause" id="pause" onClick={handleStartPause}>
+            Pause
+          </button>
+          <button data-testid="reset" id="reset" disabled>
+            Reset
+          </button>
+        </>
+      ) : (
+        <>
+          <button data-testid="start" id={timer > 0 ? 'resume' : 'start'} onClick={handleStartPause}>
+            {timer > 0 ? 'Resume' : 'Start'}
+          </button>
+          <button data-testid="reset" id="reset" onClick={handleReset} disabled={timer === 0}>
+            Reset
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+const formatTime = time => {
+  const hours = Math.floor(time / 3600).toString().padStart(2, '0');
+  const minutes = Math.floor((time % 3600) / 60).toString().padStart(2, '0');
+  const seconds = (time % 60).toString().padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
+};
+
+export default Stopwatch;
